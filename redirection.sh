5e2e860c802c14fc3489bd405d0a094c6b04c688
Guru Theja <gurutejashetty@gmail.com>
2025-08-05 16:43:23 +0530

    Autopush

--- redirection.sh
+++ redirection.sh
@@ -4,13 +4,13 @@ ACTION=$1
 
 case $ACTION in
     start)
-        echo "Payments services started"
+        echo -e "\e[32m Payments services started \e[0m"
         ;;
     stop)
-        echo "Payment services stopped"
+        echo -e "\e[34m Payment services stopped \e[0m"
         ;;
     restart)
-        echo "Payments services resarted"
+        echo -e "\e[35m Payments services resarted \e[0m"
         ;;
     *)
         echo "Valid options are start or stop or restart"
