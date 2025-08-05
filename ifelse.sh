c8e2aa9e9446c659d430004a2a2214cd2341c4dc
Guru Theja <gurutejashetty@gmail.com>
2025-08-05 17:33:42 +0530

    Autopush

--- ifelse.sh
+++ ifelse.sh
@@ -8,7 +8,17 @@ if [ "$ACTION" == "Start" ]; then
 
     echo "Payment Services started"
     exit 0
-else
-    echo "Valid option is start"
+
+else if ["$ACTION" == "stop"]; then
+
+    echo "Payment option is stop"
     exit 1
+
+else if ["$ACTION" == "restart"]; then
+
+    echo "Payment service restarted"
+    exit 2
+else
+    echo "ALL OK"
+    exit 3
 fi
