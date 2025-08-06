a16128c585064dee242d613fc07a722f452f931f
Guru Theja <gurutejashetty@gmail.com>
2025-08-06 16:52:09 +0530

    Autopush

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -47,6 +47,39 @@ systemctl start mongod &>> ${LOGFILE}
 
 stat $?
 
+echo -n "Download the ${COMPONENT} schema:"
+
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+stat $?
+
+echo -n "Extracting the ${COMPONENT} schema:"
+
+cd /tmp
+unzip mongodb.zip &>> ${LOGFILE}
+
+stat $?
+
+
+echo -n "Inject the ${COMPONENT} schema:"
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
+
+stat $?
+
+echo -e -n "\e[33 ${COMPONENT} installation is completed \e[0m"
+
+
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
+
 
 <<COMMENT
 echo -n "Downloading the ${COMPONENT} component"
