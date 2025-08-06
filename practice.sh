655cf19a4a2f476e9549e9733983b82cbcf5b46e
Guru Theja <gurutejashetty@gmail.com>
2025-08-06 11:33:06 +0530

    Autopush

--- practice.sh
+++ practice.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
-echo "if usage"
+for val in 10 20 30 40 50 ; do
 
-ACTION=$1
-
-if [ "${ACTION}" == "start" ]; then
-
-    echo "Payment services started"
-    exit 0
-
-elif [ "${ACTION}" == "stop" ]; then
-
-    echo "Payment services stopped"
-    exit 1
-
-elif [ "${ACTION}" == "restart" ]; then
-
-    echo "Payment services re-started"
-    exit 2
-
-else 
-
-    echo "Select Valid option start, stop or restart"
-    exit 3
-fi
+    echo "value for the loop ${val}"
+    
+done
