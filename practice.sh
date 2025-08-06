b4b72aaf27cce1dd4c7b1606d99006d06c5869a8
Guru Theja <gurutejashetty@gmail.com>
2025-08-06 11:07:48 +0530

    Autopush

--- practice.sh
+++ practice.sh
@@ -5,10 +5,14 @@ ACTION=$1
 case ${ACTION} in
     start)
         echo "Start the payment service"
+        ;;
     stop)
         echo "stop the payment service"
+        ;;
     restart)
         echo "restart the payment service"
+        ;;
     *)
         echo "Please select correct option start, stop or restart"
+        ;;
 esac
