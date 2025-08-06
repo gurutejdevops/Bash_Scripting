e79ece7bfeadd149eda7624595bd206f96ce3b49
Guru Theja <gurutejashetty@gmail.com>
2025-08-06 11:22:30 +0530

    Autopush

--- practice.sh
+++ practice.sh
@@ -4,9 +4,23 @@ echo "if usage"
 
 ACTION=$1
 
-if [ "${ACTION}" == "start" ] ; then
+if [ "${ACTION}" == "start" ]; then
 
     echo "Payment services started"
-else
-    echo "Valid option is start only"
+    exit 0
+
+elif [ "${ACTION}" == "stop" ]; then
+
+    echo "Payment services started"
+    exit 1
+
+elif [ "${ACTION}" == "restart" ]; then
+
+    echo "Payment services started"
+    exit 2
+
+else 
+
+    echo "Select Valid option start, stop or restart"
+    exit 3
 fi
