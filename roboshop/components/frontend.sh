7d3db974ff72a6239c571986942677c3f9bf4abf
Guru Theja <gurutejashetty@gmail.com>
2025-08-06 12:38:53 +0530

    Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+#Validate the user
+
+USER_ID = $(id -u)
+
+if [ ${USER_ID} -ne 0 ]; then
+
+    echo -e "\e[31m script is expected to execute by the root user or with a sudo privilege \e[0m \n \t Example: \n\t sudo wrapper.sh frontend.sh"
+    exit 1
+fi
+
 echo "I am Fronend"
+
+yum install nginx -y
