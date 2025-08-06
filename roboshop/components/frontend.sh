ebd2378ee5e5dd8c2482cb6820e96e9be7a20ef4
Guru Theja <gurutejashetty@gmail.com>
2025-08-06 12:41:02 +0530

    Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,9 +2,9 @@
 
 #Validate the user
 
-USER_ID = $(id -u)
+USER_ID="$(id -u)"
 
-if [ ${USER_ID} -ne 0 ]; then
+if [ ${USER_ID} -ne 0 ] ; then
 
     echo -e "\e[31m script is expected to execute by the root user or with a sudo privilege \e[0m \n \t Example: \n\t sudo wrapper.sh frontend.sh"
     exit 1
