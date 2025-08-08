0f69fea7ee793c8150581a2fe814bbab8878106b
Guru Theja <gurutejashetty@gmail.com>
2025-08-08 15:57:18 +0530

    Autopush

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -14,7 +14,7 @@ if [ "${USER_ID}" != "root" ]; then
 fi
 
 stat() {
-        if [$1 -eq 0]; then
+        if [ $1 -eq 0 ]; then
             echo -e "\e[32m success \e[0m"
         else
             echo -e "\e[31m Failure \e[0m"
