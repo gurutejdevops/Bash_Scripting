72ddf433b52228eb8f38012dda8e528ac579b872
Guru Theja <gurutejashetty@gmail.com>
2025-08-08 15:58:16 +0530

    Autopush

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -18,7 +18,7 @@ stat() {
             echo -e "\e[32m success \e[0m"
         else
             echo -e "\e[31m Failure \e[0m"
-        exit 2
+            exit 2
 }
 
 echo -e -n "\e[33m Configuring ${COMPONENT} repo \e[0m"
