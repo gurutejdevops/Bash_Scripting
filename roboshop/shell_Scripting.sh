fd48bb74a57bfe41fc9ebd585135f4c79d385341
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 12:46:56 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -12,3 +12,6 @@ echo "The sum of ${num1} and ${num2} is ${sum}"
 #method2
 diff=$(( num1 - num2 ))
 echo "The diff of ${num1} and ${num2} is ${diff}"
+#method3
+product=$ [ num1 * num2 ]
+echo "The product of ${num1} and ${num2} is ${product}"
