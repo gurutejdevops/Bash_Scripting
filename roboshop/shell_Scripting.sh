0a818bab7867f78bda248dac34dbfa8a4f1a1d0b
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 13:04:04 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -6,7 +6,7 @@
 # Date Modified:
 num1=200
 num2=300
-if [ num1 -gt num2 ]; then
+if [ ${num1} -gt ${num2} ]; then
     echo "${num1} is greater than ${num2}"
 else
     echo "${num1} is less than ${num2}"
