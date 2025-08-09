2f93aadcb0b7133ad8abbc8639ac9627a19b7308
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 14:16:54 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -4,16 +4,9 @@
 # Date Created:
 # Version: 
 # Date Modified:
-echo "Hello there, Welcome to \"Deloitte Consulting Inc\" "
-read -p "enter name:" name
-read -p "enter username:" usrname
-read -p "enter desg:" desg
-read -p "enter shelltype:" shelltype
-
-sudo useradd -s $shelltype -c $desg -m -d /home/$usrname $name
-
-if [ $? -eq 0 ]; then
-    echo "User created successfully"
-else
-    echo "Check with Admin"
-fi
+num=10
+for((i=0;i<$num;i++))
+do
+echo "Hi Guru ${i}"
+done
+echo "End of loop"
