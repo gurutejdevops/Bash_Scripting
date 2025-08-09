3b9f0005c2d1964acddee09c89143901d47e116b
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 12:57:12 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -15,5 +15,5 @@ echo "The diff of ${num1} and ${num2} is ${diff}"
 #method3
 product=$((num1 * num2))
 echo "The product of ${num1} and ${num2} is ${product}"
-result=$((num1/num2))
+result=$((expr $num1/num2))
 echo "The product of ${num1} and ${num2} is ${result}"
