bc498b83bf71be81a6797b6cda585e370c480471
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 13:23:03 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -7,9 +7,11 @@
 read -p "Enter the number1:" num1
 read -p "Enter the number2:" num2
 read -p "Enter the number3:" num3
-if [[ $num1 -gt $num2 && $num1 -gt $num3 ]] then
+if [[ $num1 -gt $num2 && $num1 -gt $num3 ]] 
+then
     echo "$num1 is greater"
-elif [[ $num2 -gt $num1 && $num2 -gt $num3 ]] then
+elif [[ $num2 -gt $num1 && $num2 -gt $num3 ]] 
+then
     echo "$num2 is greater"
 else
     echo "$num3 is greater"
