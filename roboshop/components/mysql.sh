d4ae1b5ad61903f10e74fdbadc0e092506b4bfb5
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 21:50:54 +0530

    Autopush

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -88,7 +88,7 @@ stat $?
 
 echo "show databases;" | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo -n "Default ${COMPONENT} password reset:"
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD} &>> ${LOGFILE}
     stat $?
