2d73f7cac1c46b7403d029f56c07061edb718617
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 13:57:59 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -4,11 +4,14 @@
 # Date Created:
 # Version: 
 # Date Modified:
-case $UID in 
-	0) 
-		echo "You are root user";;
-    1000)
-		echo "You are ubuntu user";;
-	*)
-		echo "Are you a new user??";;
+case $1 in
+    1)
+        echo "welcome Linux sesions";;
+    2)
+        sudo yum update;;
+    3)
+        sudo yum install nginx -y;;
+    *)
+        echo "Please enter a correct value"
+		echo "./script12.sh 1/2/3/4 (1: greet | 2: update apt |3: install java | 4: install tree)";;
 esac
