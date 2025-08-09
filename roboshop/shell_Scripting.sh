a5e9f25186812cab6872e798d8a96eb201e076aa
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 13:26:41 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -4,15 +4,15 @@
 # Date Created:
 # Version: 
 # Date Modified:
-read -p "Enter the number1:" num1
-read -p "Enter the number2:" num2
-read -p "Enter the number3:" num3
-if [[ $num1 -gt $num2 && $num1 -gt $num3 ]] 
-then
-    echo "$num1 is greater"
-elif [[ $num2 -gt $num1 && $num2 -gt $num3 ]] 
-then
-    echo "$num2 is greater"
-else
-    echo "$num3 is greater"
-fi
+echo $0        #Name of your script
+echo "===="
+echo $1        # first argument
+echo $2        # second argument
+echo $3
+echo $4
+echo $5
+echo "==="
+echo $#       # the count of number of arguments
+echo $*       # a list of all the arguments we passed
+echo $$       # running process id
+echo $?       # return value  - earlier command successful/unsuccessful
