e65884e01241469a7a1ef529715490cf5e0d5062
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 13:13:54 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -6,9 +6,9 @@
 # Date Modified:
 read -p "Hello there, please enter your name: " name
 read -sp "Please enter your age: "  age 
-echo "\n the age is ${age}"
+echo -e "\n the age is ${age}"
 read -p "Course Interested: " course
 read -p "Qualification: "  qual
 read -p "Years of experience: " experience
 read -sp "Mobile for communication: " mobile
-echo "\n the contact number is ${mobile}"
+echo -e "\n the contact number is ${mobile}"
