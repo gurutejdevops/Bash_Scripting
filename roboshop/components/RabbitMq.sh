ce74838bc392e2091ba6d019daebfd32cf12d554
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 23:38:09 +0530

    Autopush

--- roboshop/components/RabbitMq.sh
+++ roboshop/components/RabbitMq.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
 
-echo "I am RabbitMQ"
+USER_ID="$(whoami)"
+COMPONENT=mongodb
+LOGFILE=/tmp/${COMPONENT}.log
+
+if [ ${USER_ID} != root ] ; then
+
+    echo -e "\e[31m script is expected to execute by the root user or with a sudo privilege \e[0m 
+    \n \t Example: \n\t sudo wrapper.sh ${COMPONENT}.sh"
+    exit 1
+fi
+
+stat() {
+    if [ $1 -eq 0 ]; then
+
+        echo -e "\e[32m success \e[0m"
+    else
+
+        echo -e "\e[31m Failure \e[0m"
+    fi 
+
+}
+
+
+echo -n "Erlang is a dependency that is needed for RabbitMQ:"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash   &>> ${LOGFILE}
+stat $?
+
+echo -n "Setup YUM repositories for RabbitMQ:"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash   &>> ${LOGFILE}
+stat $?
+
+echo -n "Install RabbitMQ:"
+yum install rabbitmq-server -y  &>> ${LOGFILE}
+stat $?
+
+echo -n "Start RabbitMQ:"
+systemctl enable rabbitmq-server 
+systemctl start rabbitmq-server
+systemctl status rabbitmq-server -l
+stat $?
+
+echo -n "Create Application User:"
+rabbitmqctl add_user ${APPUSER} roboshop123
+rabbitmqctl set_user_tags ${APPUSER} administrator
+rabbitmqctl set_permissions -p / ${APPUSER} ".*" ".*" ".*"
+stat $?
