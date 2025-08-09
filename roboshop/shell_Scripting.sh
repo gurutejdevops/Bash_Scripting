d6a7fc86317d1ea61dbac8ebf1a5070ac1a5d056
Guru Theja <gurutejashetty@gmail.com>
2025-08-09 13:35:22 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -4,15 +4,14 @@
 # Date Created:
 # Version: 
 # Date Modified:
-echo $0        #Name of your script
-echo "===="
-echo $1        # first argument
-echo $2        # second argument
-echo $3
-echo $4
-echo $5
-echo "==="
-echo $#       # the count of number of arguments
-echo $*       # a list of all the arguments we passed
-echo $$       # running process id
-echo $?       # return value  - earlier command successful/unsuccessful
+case $UID in
+    0)
+        echo "ur  root user"
+        ;;
+    1000)
+        echo "ur ubuntu user"
+        ;;
+    *)
+        echo "new user"
+        ;;
+esac
