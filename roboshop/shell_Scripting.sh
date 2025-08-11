4dd09a585fe296f1fa86c760c305ee469a34bcc0
Guru Theja <gurutejashetty@gmail.com>
2025-08-11 17:13:41 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -14,10 +14,11 @@ if [ -z $1 ]; then
     exit 1
 fi 
 
-AMI_ID=ami-0ad3d11156336bf7f
+#AMI_ID=ami-0ad3d11156336bf7f
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=aws-parallelcluster-3.7.1-centos7-hvm-x86_64-202309151532 2023-09-15T15-36-15.606Z" | jq ".Images[].ImageId" | sed -e 's/"//g')"
 Instance_tpe=t2.micro
 Key_Name=New_pem_aws_key
-security_group=sg-067331603ab07b024
+security_group="$(aws ec2 describe-security-groups --filters Name=group-name,Values=Practice-Allow-all | jq ".SecurityGroups[].GroupId"| sed -e 's/"//g')"
 
 PRIVATE_IP=$(aws ec2 run-instances \
     --image-id ${AMI_ID} \
