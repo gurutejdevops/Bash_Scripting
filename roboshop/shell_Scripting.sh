5135b4fc22915e563124e1c923f838f62714a0ad
Guru Theja <gurutejashetty@gmail.com>
2025-08-12 15:15:35 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -2,14 +2,16 @@
 
 ACTION=$1
 
-case ${ACTION} in
-    start)
-        echo -e "\e[31m Payment Services started \e[0m"
-        ;;
-    stop)
-        echo -e "\e[32m Payment services stopped \e[0m"
-        ;;
-    *)
-        echo -e "\e[32m Please select start or stop services \e[0m"
-        ;;
-esac
+if [ "${ACTION}" == "start" ]; then
+
+    echo -e "\e[31m Payment Services started \e[0m"
+
+elif [ "${ACTION}" == "stop" ]; then
+
+    echo -e "\e[31m Payment Services stopped \e[0m"
+
+else
+
+    echo -e "\e[32m Please select start or stop services \e[0m"
+
+fi
