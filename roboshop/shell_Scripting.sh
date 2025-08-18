2f58b852124cd825321384f459c53b25b8f724d7
Guru Theja <gurutejashetty@gmail.com>
2025-08-18 10:40:05 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -5,6 +5,12 @@
 #sh <scriptname.sh>
 #./<scriptname.sh>
 
+a=10
+b=10
+
 read -p "Enter the name:" name
 echo "name is ${name}"
+echo "The value of a is ${a}"
+echo "The value of b is ${b}"
+echo "The value of b is ${x}"
 echo -e "\e[43;32m Welcome to \nScript learning \e[0m"
