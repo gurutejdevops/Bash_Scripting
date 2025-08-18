d10e4e98ae394523ff1b24f69f5fc57e185d89c4
Guru Theja <gurutejashetty@gmail.com>
2025-08-18 10:44:47 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -7,10 +7,14 @@
 
 a=10
 b=10
+date="${date +%D}"
+dir="${pwd}"
+session_count="${who | wc -l}"
 
-read -p "Enter the name:" name
-echo "name is ${name}"
+echo "\e[30m The date is ${date}\e[0m"
+echo "\[31m The current dir is ${dir} \e[0m"
+echo "The session_count is ${session_count}"
 echo "The value of a is ${a}"
 echo "The value of b is ${b}"
 echo "The value of b is ${x}"
-echo -e "\e[43;32m Welcome to \nScript learning \e[0m"
+echo -e "\e[32m Welcome to \nScript learning \e[0m"
