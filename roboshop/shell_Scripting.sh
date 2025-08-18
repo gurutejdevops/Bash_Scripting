41dbc57464d2b54fe643c11fbc8395e9aa65abe1
Guru Theja <gurutejashetty@gmail.com>
2025-08-18 10:56:59 +0530

    Autopush

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -10,10 +10,18 @@ b=10
 date="$(date +%D)"
 dir="$(pwd)"
 session_count="$(who | wc -l)"
+argument1=$1
+argument2=$2
 
 echo "The date is ${date}"
 echo "The current dir is ${dir}"
 echo "The session_count is ${session_count}"
+echo "The argument1 is ${argument1}"
+echo "The argument2 is ${argument2}"
+echo "The file name is $0 "
+echo "The PID is $$"
+echo "The no of arguments is: $#"
+echo "The arguments are $*"
 echo "The value of a is ${a}"
 echo "The value of b is ${b}"
 echo "The value of b is ${x}"
