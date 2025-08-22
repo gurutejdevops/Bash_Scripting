5fdda13db72e329fd5ca6ed36d6928d538e9ef15
Guru Theja <gurutejashetty@gmail.com>
2025-08-22 14:32:29 +0530

    Autopush

--- sample_file1.sh
+++ sample_file1.sh
@@ -6,15 +6,16 @@ USER_ID=$(whoami)
 stat()
 {
     if [ $1 -eq 0 ]; then
-        echo "\e[32m Success \e[0m"
+        echo -e "\e[32m Success \e[0m"
     else
-        echo "\e[34m failure \e[0m"
+        echo -e "\e[34m failure \e[0m"
     fi
 }
 
 echo -n "Root access checking:"
 
 if [ ${USER_ID} != root ]; then
+
     echo "The run the script with sudo access"
 else
     echo "script ran by root user"
