6b7e0ee07ae4670fae82b69df552fa6101daecd6
Guru Theja <gurutejashetty@gmail.com>
2025-08-22 14:46:19 +0530

    Autopush

--- sample_file1.sh
+++ sample_file1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USER_ID=$(whoami)
+APP_USER=roboshop
 
 
 stat()
@@ -15,6 +16,18 @@ stat()
 echo -e -n "\e[33m Installing nginx \e[0m"
 
 yum install nginx -y &>> LOGFILE.txt
+
+stat $?
+
+echo -n "Checking the application user:"
+
+id ${APP_USER}
+
+if [ $? -eq 0 ]; then
+    echo "Application user is available"
+else
+    useradd ${APP_USER}
+fi
 
 stat $?
 
