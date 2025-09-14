a79658c3871485dcde87fc4e62e321b121855cd8
Guru Theja <gurutejashetty@gmail.com>
2025-09-14 14:56:41 +0530

    Autopush

--- practice_unix.sh
+++ practice_unix.sh
@@ -2,4 +2,4 @@
 echo "Hello World"
 echo -e "Welcome to \n shell scripting practice \n \t ok bye"
 echo -e "\e[32m This is Guru Theja \e[0m"
-echo -e "\e[40, 32m This is Satish from chittor \e[0m"
+echo -e "\e[41, 32m This is Satish from chittor \e[0m"
