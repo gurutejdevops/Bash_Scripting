da831a51d193998b9327bf9b389c321aa3bac1b0
Guru Theja <gurutejashetty@gmail.com>
2025-09-14 15:00:26 +0530

    Autopush

--- practice_unix.sh
+++ practice_unix.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Hello World"
-echo -e "Welcome to \n shell scripting practice \n \t ok bye"
-echo -e "\e[32m This is Guru Theja \e[0m"
-echo -e "\e[41;32m This is Satish from chittor \e[0m"
+a=10
+b=20
+c=xyz
+echo -e "\e[33m The value of a is: ${a} \e[0m"
+echo -e "\e[32m The value of b is: ${b} \e[0m"
+echo -e "\e[43;32m The value of c is: ${c} \e[0m"
+echo -e "\e[32m The value of x is: ${x} \e[0m"
