8c3a106a5038ee7f875d15de1bb152d4e79b9d6d
Guru Theja <gurutejashetty@gmail.com>
2025-09-14 15:06:25 +0530

    Autopush

--- practice_unix.sh
+++ practice_unix.sh
@@ -7,3 +7,11 @@ echo -e "\e[33m The value of a is: ${a} \e[0m"
 echo -e "\e[32m The value of b is: ${b} \e[0m"
 echo -e "\e[43;32m The value of c is: ${c} \e[0m"
 echo -e "\e[32m The value of x is: ${x} \e[0m"
+date="$(date +%D)"
+session_count="$(who | wc -l)"
+dir="$(pwd)"
+echo -e "\e[32m The date is ${date} \e[0m"
+echo -e "\e[32m The session_count is ${session_count} \e[0m"
+echo -e "\e[33m The dir is ${dir} \e[0m"
+
+
