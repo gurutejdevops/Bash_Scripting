9a8950ef49e1246397f5cf8cfa10d9f05b8bec22
Guru Theja <gurutejashetty@gmail.com>
2025-10-20 17:24:38 +0530

    Auto commit

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -97,3 +97,16 @@ fi
 COMMENT
 
 echo "Hai from Comments"
+
+num1=20
+num2=30
+num3=40
+
+if [[ $num1 -gt $num2 && $num1 -gt $num3 ]]; then
+    echo "The ${num1} is greater than ${num2} and ${num3}"
+elif [[ $num2 -gt $num1 && $num2 -gt $num3 ]]; then
+    echo "The ${num2} is greater than ${num1} and ${num3}"
+else
+    echo "The ${num3} is greater than ${num1} and ${num2}"
+fi
+
