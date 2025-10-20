8d2894fda9e08cdf274f9f5606574d4ed651ad5d
Guru Theja <gurutejashetty@gmail.com>
2025-10-20 13:30:47 +0530

    Auto commit

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -11,20 +11,24 @@
 # echo -e "\e[33m ala kadhu raa \n erripuka \n tension akuva tesukovaddu ra \e[0m"
 # echo -e "\e[42;33m Good for health \e[0m"
 
-# Variables
-a=10
-b=20
-c=30
+# # Variables
+# a=10
+# b=20
+# c=30
 
-echo -e "\e[32m The value of a is ${a} \e[0m"
-echo -e "\e[33m The value of b is ${b} \e[0m"
-echo -e "\e[34m The value of c is ${c} \e[0m"
-echo -e "\e[35m The value of y is ${y} \e[0m"
+# echo -e "\e[32m The value of a is ${a} \e[0m"
+# echo -e "\e[33m The value of b is ${b} \e[0m"
+# echo -e "\e[34m The value of c is ${c} \e[0m"
+# echo -e "\e[35m The value of y is ${y} \e[0m"
 
-date="$(date)"
-session_count="$(who | wc -l)"
-dir="$(pwd)"
+# date="$(date)"
+# session_count="$(who | wc -l)"
+# dir="$(pwd)"
 
-echo -e "\e[32m The date is ${date} \e[0m"
-echo -e "\e[33m The session_count is ${session_count} \e[0m"
-echo -e "\e[34m The current dir is ${dir} \e[0m"
+# echo -e "\e[32m The date is ${date} \e[0m"
+# echo -e "\e[33m The session_count is ${session_count} \e[0m"
+# echo -e "\e[34m The current dir is ${dir} \e[0m"
+
+# The Default Arguments
+
+echo -e "\e[32m The script name is $0 \e[0m"
