0de0e4c1a09d2631a1942f5d887512730d13d730
Guru Theja <gurutejashetty@gmail.com>
2025-10-20 13:04:13 +0530

    Auto commit

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -5,8 +5,17 @@
 #sh <scriptname.sh>
 #./<scriptname.sh>
 
-echo $SHELL
-echo "Welcome to Linux Shell Scripting practice again"
-echo -e "\e[32m welcome to \n Linux Shell Scripting \n practice again \e[0m"
-echo -e "\e[33m ala kadhu raa \n erripuka \n tension akuva tesukovaddu ra \e[0m"
-echo -e "\e[42;33m Good for health \e[0m"
+# echo $SHELL
+# echo "Welcome to Linux Shell Scripting practice again"
+# echo -e "\e[32m welcome to \n Linux Shell Scripting \n practice again \e[0m"
+# echo -e "\e[33m ala kadhu raa \n erripuka \n tension akuva tesukovaddu ra \e[0m"
+# echo -e "\e[42;33m Good for health \e[0m"
+
+# Variables
+a=10
+b=20
+c=30
+
+echo -e "\e[32m The value of a is ${a} \e[0m"
+echo -e "\e[33m The value of b is ${b} \e[0m"
+echo -e "\e[34m The value of c is ${c} \e[0m"
