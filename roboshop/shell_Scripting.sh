fe654b82dd8ee7489bfbee7d88d56700ec83e39e
Guru Theja <gurutejashetty@gmail.com>
2025-10-20 13:13:24 +0530

    Auto commit

--- roboshop/shell_Scripting.sh
+++ roboshop/shell_Scripting.sh
@@ -19,4 +19,4 @@ c=30
 echo -e "\e[32m The value of a is ${a} \e[0m"
 echo -e "\e[33m The value of b is ${b} \e[0m"
 echo -e "\e[34m The value of c is ${c} \e[0m"
-echo -e "\e[35m The value of x is ${x} \e[0m"
+echo -e "\e[35m The value of y is ${y} \e[0m"
