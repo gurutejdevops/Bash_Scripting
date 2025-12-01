9e713395a0dd50e67814f0b74d60f36066a042da
Guru Theja <gurutejashetty@gmail.com>
2025-12-01 20:10:46 +0530

    Auto commit

--- practice_shell.sh
+++ practice_shell.sh
@@ -2,3 +2,12 @@ echo "Welcome to Shell Scripting"
 echo -e "Welcome to \n shell scripting \t Guru"
 echo -e "\e[32m This is Guru Theja \e[0m"
 echo -e "\e[43;32m This is another guru teja \e[0m"
+
+a=10
+b=10
+c=xyz
+
+echo -e "\e[32m The value of a is ${a} \e[0m"
+echo -e "\e[34m The value of b is ${b} \e[0m"
+echo -e "\e[35m The value of c is ${c} \e[0m"
+echo -e "\e[36m The value of d is ${d} \e[0m"
