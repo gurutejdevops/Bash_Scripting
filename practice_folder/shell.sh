2b210a0fcc0eefb7b7293666b9537924e2cdf34f
Guru Theja <gurutejashetty@gmail.com>
2025-12-02 15:07:45 +0530

    Auto commit

--- practice_folder/shell.sh
+++ practice_folder/shell.sh
@@ -1,7 +1,16 @@
+stat() {
+    if [ $1 -eq 0 ]; then
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[33m Failure \e[0m"
+}
+
 user_id="$(whoami)"
 
 if [ ${user_id} != root ]; then 
     echo  -e "\e[32m script expected to executef by root user or sudo access \e[0m"
 else 
-    echo "proceed with the execution"
+    echo -e "\e[34m proceed with the execution \e[0m"
 fi
+
+stat $?
